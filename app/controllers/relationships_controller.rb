6efe6aa7d6ac71fcaffd230f0624161c3af8db83
EC2 Default User <ec2-user@ip-172-31-44-63.us-east-2.compute.internal>
2018-03-23 02:39:56 +0000

    timeline

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:follow_id])
     current_user.follow(user)
-    flash[:sucesss] = 'ユーザをフォローしました。'
-    redirect_to_user
+    flash[:success] = 'ユーザをフォローしました。'
+    redirect_to user
   end
 
   def destroy
     user = User.find(params[:follow_id])
     current_user.unfollow(user)
     flash[:success] = 'ユーザのフォローを解除しました。'
-    redirect_to_user
+    redirect_to user
   end
 end
